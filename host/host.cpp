537ae648a1a9180dbc25bdba8bc19746744a5574
SHEN Jiamin <shen_jiamin@comp.nus.edu.sg>
2021-04-29 13:24:14 +0000

    generate random dataset

--- host/host.cpp
+++ host/host.cpp
@@ -1,7 +1,3 @@
-#ifndef SGX_MODE_SIM
-#define SGX_MODE_HW
-#endif
-
 #include <iostream>
 #include <map>
 #include <vector>
@@ -13,25 +9,21 @@
 
 #include "enclave.hpp"
 
-constexpr size_t TEST_SIZE = (1 << 20);
+#ifndef SGX_MODE_SIM
+#define SGX_MODE_HW
+#endif
 
-auto random_dataset() -> std::map<uint32_t, uint32_t>
+template <class KT, class VT>
+auto random_dataset(size_t size) -> std::pair<std::vector<KT>, std::vector<VT>>
 {
     PRNG<uint32_t> prng;
 
-    std::map<uint32_t, uint32_t> dataset;
+    std::pair<std::vector<KT>, std::vector<VT>> dataset;
 
-    for (size_t i = 0; i < TEST_SIZE; i++)
+    for (size_t i = 0; i < size; i++)
     {
-        auto k = prng();
-        if (dataset.find(k) == dataset.end())
-        {
-            dataset[k] = 1;
-        }
-        else
-        {
-            dataset[k]++;
-        }
+        dataset.first.push_back(prng());
+        dataset.second.push_back(prng());
     }
 
     return dataset;
@@ -39,26 +31,32 @@ auto random_dataset() -> std::map<uint32_t, uint32_t>
 
 auto main(int argc, const char* argv[]) -> int
 {
+    constexpr size_t TEST_SIZE = (1 << 8);
+
     if (argc != 2)
     {
         fprintf(stderr, "Usage: %s enclave_image_path\n", argv[0]);
         return EXIT_FAILURE;
     }
 
+    bool simulate =
 #ifdef SGX_MODE_SIM
-    HelloworldEnclave enclave(argv[1], true);
+        true;
 #else
-    HelloworldEnclave enclave(argv[1], false);
+        false;
 #endif
 
-    std::vector<uint32_t> arr = {0};
-    const size_t DATA_SIZE = 12;
-    for (size_t i = 0; i < DATA_SIZE; i++)
-    {
-        arr.push_back(1);
-    }
+    HelloworldEnclave enclave_a(argv[1], simulate);
+    HelloworldEnclave enclave_b(argv[1], simulate);
 
-    enclave.helloworld(arr);
+    auto ds1 = random_dataset<uint32_t, uint32_t>(TEST_SIZE);
+    auto ds2 = random_dataset<uint32_t, uint32_t>(TEST_SIZE);
+
+    puts("[+] enclave_a.helloworld(ds1.first);");
+    enclave_a.helloworld(ds1.first);
+
+    puts("[+] enclave_b.helloworld(ds2.first);");
+    enclave_b.helloworld(ds2.first);
 
     return 0;
 }
