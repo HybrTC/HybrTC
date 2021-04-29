38b9410744308e381697b56b2dda613f9a688e4d
SHEN Jiamin <shen_jiamin@comp.nus.edu.sg>
2021-04-29 12:03:46 +0000

    remove ocall

--- enclave/enc.c
+++ enclave/enc.c
@@ -19,15 +19,4 @@ void enclave_helloworld()
     // the fprintf function is not part of the enclave
     // as it requires support from the kernel.
     fprintf(stdout, "Hello world from the enclave\n");
-
-    // Call back into the host
-    oe_result_t result = host_helloworld();
-    if (result != OE_OK)
-    {
-        fprintf(
-            stderr,
-            "Call to host_helloworld failed: result=%u (%s)\n",
-            result,
-            oe_result_str(result));
-    }
 }
--- host/ocall.c
+++
@@ -1,9 +0,0 @@
-
-#include <stdio.h>
-
-// This is the function that the enclave will call back into to
-// print a message.
-void host_helloworld()
-{
-    fprintf(stdout, "Enclave called into host to print: Hello World!\n");
-}
