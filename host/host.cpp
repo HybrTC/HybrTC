9636f8d6c477e016eeaf1477e311814c382e5640
SHEN Jiamin <shen_jiamin@comp.nus.edu.sg>
2021-04-29 13:16:56 +0000

    turn simulation flag to a macro

--- host/host.cpp
+++ host/host.cpp
@@ -1,8 +1,13 @@
+#ifndef SGX_MODE_SIM
+#define SGX_MODE_HW
+#endif
+
 #include <iostream>
 #include <map>
 #include <vector>
 
 #include <openenclave/host.h>
+#include <boost/program_options.hpp>
 
 #include "prng.hpp"
 
@@ -36,26 +41,15 @@ auto main(int argc, const char* argv[]) -> int
 {
     if (argc != 2)
     {
-        fprintf(
-            stderr, "Usage: %s enclave_image_path [ --simulate  ]\n", argv[0]);
-
+        fprintf(stderr, "Usage: %s enclave_image_path\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    // check_simulate_opt
-    bool simulate = false;
-    for (int i = 0; i < argc; i++)
-    {
-        if (strcmp(argv[i], "--simulate") == 0)
-        {
-            fprintf(stdout, "Running in simulation mode\n");
-            memmove(&argv[i], &argv[i + 1], (argc - i) * sizeof(char*));
-            argc--;
-            simulate = true;
-        }
-    }
-
-    HelloworldEnclave enclave(argv[1], simulate);
+#ifdef SGX_MODE_SIM
+    HelloworldEnclave enclave(argv[1], true);
+#else
+    HelloworldEnclave enclave(argv[1], false);
+#endif
 
     std::vector<uint32_t> arr = {0};
     const size_t DATA_SIZE = 12;
