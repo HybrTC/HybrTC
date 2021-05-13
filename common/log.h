a244b127aac0d4fd1d5ed909f21873ab909f73ad
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2021-05-13 10:49:04 +0000

    improve enclave trace

--- common/log.h
+++ common/log.h
@@ -4,9 +4,52 @@
 #ifndef OE_SAMPLES_ATTESTATION_COMMON_LOG_H
 #define OE_SAMPLES_ATTESTATION_COMMON_LOG_H
 
+#include <cstddef>
 #include <cstdio>
+#include <sstream>
+#include <string>
+#include <thread>
 
-#define TRACE_ENCLAVE(fmt, ...) \
-    fprintf(stderr, ">>> %s(%d): " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+static auto get_thread_id() -> std::string
+{
+    std::stringstream s;
+    s << std::hex << std::this_thread::get_id();
+    return s.str();
+}
+
+template <class T>
+static auto hexdump(const T& data) -> std::string
+{
+    std::string h;
+    char buf[BUFSIZ];
+    for (auto b : data)
+    {
+        snprintf(buf, BUFSIZ, "%02hhx", b);
+        h += buf;
+    }
+    return h;
+}
+
+template <class T>
+static auto hexdump(const T* data, size_t sz) -> std::string
+{
+    std::string h;
+    char buf[BUFSIZ];
+    for (size_t i = 0; i < sz; i++)
+    {
+        snprintf(buf, BUFSIZ, "%02hhx", data[i]);
+        h += buf;
+    }
+    return h;
+}
+
+#define TRACE_ENCLAVE(fmt, ...)       \
+    fprintf(                          \
+        stderr,                       \
+        ">>> [%s] %s(%d): " fmt "\n", \
+        get_thread_id().c_str(),      \
+        __FILE__,                     \
+        __LINE__,                     \
+        ##__VA_ARGS__)
 
 #endif // OE_SAMPLES_ATTESTATION_COMMON_LOG_H
