801857241018479b8899255d86cb282fdee75b80
SHEN Jiamin <shen_jiamin@comp.nus.edu.sg>
2021-11-27 11:00:22 +0800

    change HASH template arg name

--- server/enclave/psi/hash.hpp
+++ server/enclave/psi/hash.hpp
@@ -4,21 +4,21 @@
 #include "common/types.hpp"
 #include "crypto/sha512.hpp"
 
-template <unsigned ON, class OT, class IT>
+template <unsigned HashNum, class HashType, class ElemType>
 class HASH
 {
-    INTEGER_CHECK(IT, "input");
-    INTEGER_CHECK(OT, "output");
+    INTEGER_CHECK(ElemType, "input");
+    INTEGER_CHECK(HashType, "output");
 
     constexpr static size_t HASH_BYTES = 512 / 8;
 
-    static_assert(ON <= HASH_BYTES / sizeof(OT), "too many hashes required");
+    static_assert(HashNum <= HASH_BYTES / sizeof(HashType), "too many hashes required");
 
-    using M = a8<sizeof(IT)>;
-    using H = std::array<OT, ON>;
+    using M = a8<sizeof(ElemType)>;
+    using H = std::array<HashType, HashNum>;
 
   public:
-    auto operator()(const IT& val) const -> H
+    auto operator()(const ElemType& val) const -> H
     {
         const auto& msg = *reinterpret_cast<const M*>(&val);
 
